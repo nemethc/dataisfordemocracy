6860bb4751000c75c5d1aaf7f27bf8ab70c6d460
Corban Nemeth <nemeth.corban@gmail.com>
2020-06-02 21:23:04 -0700

    build

---
+++ doingdemocraticdataanalysis/word_count.R
@@ -0,0 +1,19 @@
+library(wordcountaddin)
+
+wordcountaddin::text_stats("index.Rmd")
+
+wordcountaddin::text_stats("01-intro.Rmd")
+
+wordcountaddin::text_stats("02-tidy.Rmd")
+
+wordcountaddin::text_stats("03-pivot.Rmd")
+
+wordcountaddin::text_stats("04-repro.Rmd")
+
+wordcountaddin::text_stats("05-uncertainty.Rmd")
+
+wordcountaddin::text_stats("06-audience.Rmd")
+
+wordcountaddin::text_stats("07-application.Rmd")
+
+#total words = 254 + 1036 + 1691 + 1313 + 1236 + 749 + 888 + 241 = 7408 total words
